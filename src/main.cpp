4e13d9254f9a6107db35515f7c7e2e8d09c45b88
northTyphon <stefantiberiu10@gmail.com>
2026-03-03 12:08:26 +0200

    refactor: cleaned up backend

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,6 @@
 #include "api.h"
 #include "flights.h"
 #include <crow.h>
-#include <curl/curl.h>
-#include <curl/easy.h>
-#include <curl/multi.h>
-#include <nlohmann/json.hpp>
-#include <string>
 
 using json = nlohmann::json;
 
@@ -19,8 +14,6 @@ int main() {
     const char *adults_param = req.url_params.get("adults");
     const char *children_param = req.url_params.get("children");
     const char *stops_param = req.url_params.get("stops");
-    const char *sort_param = req.url_params.get("sort");
-    const char *class_param = req.url_params.get("cabinClass");
 
     if (!from_param || !to_param || !date_param || !adults_param) {
       return crow::response(
@@ -33,15 +26,13 @@ int main() {
     std::string adults = std::string(adults_param);
     std::string children = children_param ? std::string(children_param) : "0";
     std::string stops = stops_param ? std::string(stops_param) : "none";
-    std::string sort = sort_param ? std::string(sort_param) : "BEST";
-    std::string classParam = class_param ? std::string(class_param) : "ECONOMY";
 
     std::string url =
         "https://booking-com15.p.rapidapi.com/api/v1/flights/searchFlights?"
         "fromId=" +
         from + "&toId=" + to + "&departDate=" + date + "&stops=" + stops +
         "&pageNo=1&adults=" + adults + "&children=" + children +
-        "&sort=" + sort + "&cabinClass=" + classParam + "&currency_code=USD";
+        "&sort=BEST&cabinClass=ECONOMY&currency_code=USD";
     std::string result = makeGetRequest(url);
     return parseFlight(result);
   });
